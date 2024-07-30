c248664c3112c680a5244c00d69176492584541e
Syaroful <syaroful10403@gmail.com>
2024-07-30 19:27:19 +0700

    feat: category implementation

--- src/js/script.js
+++ src/js/script.js
@@ -25,35 +25,3 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
 });
-
-var swiper = new Swiper(".mySwiper", {
-  pagination: {
-    el: ".swiper-pagination",
-    dynamicBullets: true,
-  },
-});
-
-window.onload = function () {
-  fetch("https://api-berita-indonesia.vercel.app/cnn/nasional/")
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      const articles = data.data.posts.slice(0, 8);
-      articles.forEach((post) => {
-        const markup =
-          '<article class="recomded-item flex flex-col mt-12 items-start justify-between px-8 md:w-1/2 xl:w-1/4"><img src="' +
-          post.thumbnail +
-          '" alt="News Thumbnail" class="recomended-img h-40 w-full object-cover object-center rounded-lg mr-2"/> <div class="h-24 my-2"> <a href="' +
-          post.link +
-          '" target="_blank" class="rekomdened-title line-clamp-3 text-xl font-medium leading-6 text-dark">' +
-          post.title +
-          '</a></div> <div class="flex items-center gap-x-2 text-sm"> <a href="#" class="font-medium text-primary">Terbaru</a> <h1>&#x2022;</h1> <time datetime="2020-03-16" class="recomended-date text-gray-500">' +
-          post.pubDate +
-          "</time></div></article>";
-        document
-          .querySelector(".articles-container")
-          .insertAdjacentHTML("beforeend", markup);
-      });
-    })
-    .catch((error) => console.error(error));
-};
